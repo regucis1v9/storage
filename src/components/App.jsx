83b7fbba63a11357d0c1e3bce7daae445f05b584
regucis1v9 <regnars.klavins@gmail.com>
2023-11-29 13:55:33 +0200

    Your commit message here

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+
+import '../css/App.css';
+
+function App() {
+  return (
+      <div className='w-screen h-screen bg-slate-600'>
+
+      </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/css/App.css",
+    "./src/components/App.jsx",
+
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
