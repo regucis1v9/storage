11431a6351a878e201fae868f1602c963839983b
Regnars Klavins <regnars.klavins@gmail.com>
2023-12-07 20:29:26 +0200

    landing page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,23 @@
-
+import React from 'react';
 import '../css/App.css';
 
 function App() {
   return (
-      <div className='w-screen h-screen bg-slate-600'>
-
+    <div className='main'>
+      <div className="loginSection">
+        <div className="title">THE BEST <div className="highlightTitle">STORAGE SOLUTION</div> YOU WILL FIND</div>
+        <div className="button">Get started</div>
       </div>
+      <div className="rightSide">
+        <div className="triangle">
+        </div>
+        <div className="square">
+          <div className="text">Kreklini</div>
+          <img className='logo' src="/images/logo.png" alt="" />
+        </div>
+      </div>
+
+    </div>
   );
 }
-
 export default App;
